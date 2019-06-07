3c3b57b3ca3bc7445ef891b9ffaf6d85c12e3284
Brook Li <absreim@github.com>
2019-06-07 12:46:11 -0400

    Insertion from csv upload now working.

--- server/api/upload.js
+++ server/api/upload.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const multer = require('multer')
 const papa = require('papaparse')
 
-const {Company, Model} = require('../db/models')
+const {Company} = require('../db/models')
 
 const storage = multer.memoryStorage()
 const upload = multer({storage: storage})
@@ -10,11 +10,30 @@ const upload = multer({storage: storage})
 router.post('/', upload.single('prices'), async (req, res, next) => {
   const file = req.file
   const fileString = file.buffer.toString()
-  console.log('string rep of file:')
-  console.log(fileString)
   const csv = papa.parse(fileString)
-  console.log('csv rep of file:')
-  console.log(csv)
+  const {data} = csv
+  try {
+    for (let row of data) {
+      const [companyId, companyName, date, price, comment] = row
+      const [upsertedCompany, _] = await Company.upsert(
+        {
+          externalId: companyId,
+          name: companyName
+        },
+        {
+          returning: true
+        }
+      )
+      await upsertedCompany.createSharePrice({
+        price,
+        date,
+        comment
+      })
+    }
+  } catch (err) {
+    next(err)
+    return
+  }
   res.sendStatus(200)
 })
 
